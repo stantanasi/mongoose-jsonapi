32a18c5b5ea738006f7526725a77530b16fa714d
Lory-Stan TANASI <stantanasi@gmail.com>
2024-09-15 05:42:33 +0200

    feat: define Mongoose JSON:API query helper
    Define custom helper methods on Mongoose queries

--- src/types/mongoose-jsonapi.types.ts
+++ src/types/mongoose-jsonapi.types.ts
@@ -1,5 +1,5 @@
-import { Document } from 'mongoose';
-import { JsonApiResource } from './jsonapi.types';
+import { Document, QueryWithHelpers } from 'mongoose';
+import { JsonApiBody, JsonApiQueryParams, JsonApiResource } from './jsonapi.types';
 
 export interface JsonApiInstanceMethods extends Document {
   toJsonApi: (
@@ -11,4 +11,27 @@ export interface JsonApiInstanceMethods extends Document {
     data: JsonApiResource;
     included: JsonApiResource[];
   }
+}
+
+export interface JsonApiQueryHelper {
+  withJsonApi: <ResultType extends DocType | DocType[] | null, DocType extends JsonApiInstanceMethods>(
+    this: QueryWithHelpers<ResultType, DocType, JsonApiQueryHelper>,
+    query: JsonApiQueryParams,
+  ) => this;
+
+  toJsonApi: <ResultType extends DocType | DocType[] | null, DocType extends JsonApiInstanceMethods & Document>(
+    this: QueryWithHelpers<ResultType, DocType, JsonApiQueryHelper>,
+    opts: {
+      baseUrl: string;
+      meta?: any;
+    },
+  ) => QueryWithHelpers<JsonApiBody, DocType, JsonApiQueryHelper>;
+
+  paginate: <DocType>(
+    this: QueryWithHelpers<JsonApiBody, DocType, JsonApiQueryHelper>,
+    opts: {
+      url: string;
+      query: JsonApiQueryParams;
+    },
+  ) => this;
 }
