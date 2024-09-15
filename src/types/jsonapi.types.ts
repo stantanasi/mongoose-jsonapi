72827ed3657047f7117b545d2a5c64f4a2e1692e
Lory-Stan TANASI <stantanasi@gmail.com>
2024-09-15 05:38:44 +0200

    feat: create JSON:API interfaces
    Create JsonApiQueryParams, JsonApiBody, JsonApiError, JsonApiIdentifier, JsonApiResource interfaces

---
+++ src/types/jsonapi.types.ts
@@ -0,0 +1,77 @@
+export interface JsonApiQueryParams {
+  include?: string;
+  fields?: {
+    [type: string]: string;
+  };
+  sort?: string;
+  page?: {
+    offset?: number;
+    limit?: number;
+  };
+  filter?: {
+    [type: string]: string;
+  };
+}
+
+
+export interface JsonApiBody {
+  jsonapi?: {
+    version: string;
+  };
+  data?: JsonApiResource | JsonApiResource[] | null;
+  included?: JsonApiResource[];
+  meta?: {
+    count: number;
+  } | any;
+  links?: {
+    first?: string;
+    prev?: string;
+    next?: string;
+    last?: string;
+  };
+  errors?: JsonApiError[];
+}
+
+export interface JsonApiError {
+  id?: string;
+  links?: {
+    about?: string;
+  };
+  status?: string;
+  code?: string;
+  title?: string;
+  detail?: string;
+  source?: {
+    pointer?: string;
+    parameter: string;
+  };
+  meta?: any;
+}
+
+export interface JsonApiIdentifier {
+  type: string;
+  id: string;
+}
+
+export interface JsonApiResource {
+  type: string;
+  id?: string;
+  links?: {
+    self?: string;
+    related?: string;
+  };
+  attributes?: {
+    [attribute: string]: any;
+  };
+  relationships?: {
+    [relationship: string]: {
+      links?: {
+        self?: string;
+        related?: string;
+      };
+      data?: JsonApiIdentifier | JsonApiIdentifier[];
+      meta?: any;
+    }
+  };
+  meta?: any;
+}
