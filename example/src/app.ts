29b04574cdaeb5fc41e61d3be8d4fa891f464833
Lory-Stan TANASI <stantanasi@gmail.com>
2024-09-16 14:16:02 +0200

    feat(example): connect express app to Mongoose

--- example/src/app.ts
+++ example/src/app.ts
@@ -1,5 +1,9 @@
 import cors from 'cors'
+import dotenv from 'dotenv'
 import express from 'express'
+import { connect } from 'mongoose'
+
+dotenv.config()
 
 const app = express()
 
@@ -8,6 +12,15 @@ app.use(express.json({ limit: '50mb' }))
 app.use((_req, res, next) => {
   res.header('Content-Type', 'application/vnd.api+json')
   next()
+})
+
+app.use(async (_req, _res, next) => {
+  try {
+    await connect(process.env.MONGO_DB_URI || 'mongodb://localhost/mongoose-jsonapi-example')
+    next()
+  } catch (err) {
+    next(err)
+  }
 })
 
 const port = +(process.env.PORT || 5000)
